bb8328158f00a625be01201c6a0ae589e1ecb32b
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-14 08:29:48 +0330

    TemplateView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import indexView
+from . import views
 from django.views.generic import TemplateView
 urlpatterns = [
-    path('fbv-index', indexView, name="fbv+index"),
-    path('cbv-index', TemplateView.as_view(template_name="index.html", extra_context={"name":"ali"}))
+    #path('fbv-index', indexView, name="fbv+index"),
+    #path('cbv-index', TemplateView.as_view(template_name="index.html", extra_context={"name":"ali"}))
+     path('cbv-index', views.IndexView.as_view(),name="index")
 ]
