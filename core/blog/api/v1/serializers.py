e67d1bab2d6a828e87be04b06a5c682629177af1
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-25 11:21:22 +0330

    6-15- انواع وابسته به درخواست های درونی serializer و to_representaion

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -4,27 +4,25 @@ from ...models import Post, Category
 # class PostSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
-class PostSerializer(serializers.ModelSerializer):
-    snippet = serializers.ReadOnlyField(source="get_snippet")
-    relative_path = serializers.URLField(source="get_absolute_api_url", read_only=True)
-    class Meta:
-        model = Post
-        fields = [
-            "id",
-            "author",
-            "image",
-            "title",
-            "content",
-            "snippet",
-            "category",
-            "status",
-            "relative_path",
-            "created_date",
-            "published_date",
-        ]
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id','name']
+
+class PostSerializer(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source="get_snippet")
+    relative_path = serializers.URLField(source="get_absolute_api_url", read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+    #category = serializers.SlugRelatedField(many=False,slug_field='name', queryset = Category.objects.all() )
+    class Meta:
+        model = Post
+        fields = ["id","author","image","title", "content","snippet","category","status","absolute_url","relative_path","created_date","published_date",
+            ]
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+
+    def to_representation(self, instance):
+        return super().to_representation(instance)
+
